599a166bd7ed17bf712bb629f111fcccdfdfe719
gpsndlhc <giulia.paggi@cern.ch>
2024-01-19 11:54:55 +0100

    clean up code

--- VetoEff.py
+++ VetoEff.py
@@ -2,7 +2,6 @@ from ROOT import TH1, TH1D, TH1F, TCanvas,TFile, gROOT, gDirectory, gSystem, TGr
 import numpy as np
 import ROOT
 import os, sys
-import time as t
 from glob import glob
 import csv
 
@@ -20,82 +19,60 @@ def return_bar(map, tofpet_id, tofpet_channel):
             return int(row[0])
     return -1
 
-def compute_time(map, board_id, tofpet_id, tofpet_channel, tfine, tac):  
-    a = 0
-    b = 0
-    c = 0  
-    for row in map:
-        if (row[0] == str(board_id) and row[1] == str(tofpet_id) and row[2] == str(tofpet_channel)  and row[3] == str(tac)):
-            a = float(row[4])
-            b = float(row[5]) 
-            c = float(row[6])
-            time = (-b-(b**2-(4*a*(c-333)))**0.5)/(2*a) #https://gitlab.cern.ch/snd-scifi/software/-/wikis/Raw-data-format
-            return time 
-    print('oh no')
-    return -1
-
-#DS_PROPSPEED = 14.3 #cm/ns  clock cycle = 160000000 MHz -> 6,25*10^(-12) s-> 6,25*10^-3 
-DS_SPEED = .2 #channels per tfine units
-CLK2NS = 6.25
-
-#print(str(sys.argv[1]))
+#############
+# read data #
+#############
 
 runN = sys.argv[1]
 runDirectory = f"/eos/experiment/sndlhc/raw_data/commissioning/veto/run_{runN}/"
 
 
-
-mapDS = read_csv_file("./SiPMmaps/DS_SiPM_mapping.csv")
-mapVeto = read_csv_file("./SiPMmaps/Veto_SiPM_mapping.csv")
-calibration = read_csv_file(f"{runDirectory}qdc_cal.csv")
-
-
-
-
-# read data 
 data = ROOT.TChain("data")
-
-
-
-#prepare output file
-filename = f"output_run{runN}.root"
-outfile = ROOT.TFile.Open(filename, "RECREATE")
 
 filelist = glob(f"{runDirectory}data_all*.root")
 
 for file in filelist:
     data.Add(file)
 
+# read mapping cvs
+mapDS = read_csv_file("./SiPMmaps/DS_SiPM_mapping.csv")
+mapVeto = read_csv_file("./SiPMmaps/Veto_SiPM_mapping.csv")
+calibration = read_csv_file(f"{runDirectory}qdc_cal.csv")
 
-Nentries = data.GetEntries()
+#prepare output file
+filename = f"output_run{runN}.root"
+outfile = ROOT.TFile.Open(filename, "RECREATE")
 
-#create histos 
+#################
+# Define histos #
+#################
+
 DSn_bins = 63
 DSx_min = -.5
 DSx_max = 62.5
+
+# Hits info 
 DSVHits = ROOT.TH1D("DSVHits", "DS Vertical Hits", DSn_bins, DSx_min, DSx_max) 
 DSHHits = ROOT.TH1D("DSHHits", "DS Horizontal Hits", DSn_bins, DSx_min, DSx_max)
 VetoHits = ROOT.TH1D("VetoHits", "Veto Hits", DSn_bins, DSx_min, DSx_max)
 All_VetoHitMultiplcity = ROOT.TH1D("All_VetoHitMultiplcity", "Veto Hit Multiplcity in all events", 20, -.5, 19.5)
 Cosmic_VetoHitMultiplcity = ROOT.TH1D("Cosmic_VetoHitMultiplcity", "Veto Hit Multiplcity in cosmic ray events", 20, -.5, 19.5)
 
+# qdc
 DSLQdc = ROOT.TH1D("DSLQdc", "DSLQdc", DSn_bins, DSx_min, DSx_max)
 DSRQdc = ROOT.TH1D("DSRQdc", "DSRQdc", DSn_bins, DSx_min, DSx_max)
 DSVQdc = ROOT.TH1D("DSVQdc", "DSVQdc", DSn_bins, DSx_min, 150)
 VetoQdc = ROOT.TH1D("VetoQdc", "VetoQdc", 83, -20.5, DSx_max)
 VetoQdcPerBar = ROOT.TH2D("VetoQdcPerBar", "VetoQdcPerBar", 83, -20.5, DSx_max, 83, -20.5, DSx_max) 
-VetoQDCPerChannel = ROOT.TH2D("VetoQDCPerChannel", "VetoQDCPerChannel; veto channel; qdc ", 83, -20.5, DSx_max, 83, -20.5, DSx_max)
+VetoQDCPerChannel = ROOT.TH2D("VetoQDCPerChannel", "VetoQDCPerChannel; veto channel; qdc ", DSn_bins, DSx_min, DSx_max, 83, -20.5, DSx_max)
+Cosmic_VetoQdc = ROOT.TH1D("VetoQdc", "VetoQdc", 83, -20.5, DSx_max)
 
-TimeDiff = ROOT.TH1D("TimeDiff", "Time Difference Distribution; Ltime-Rtime; entries",  41, -20.5, 20.5)
+# Efficiency
 Denominatore = ROOT.TH2D("Denominatore", "Denominatore", DSn_bins, DSx_min, DSx_max, DSn_bins, DSx_min, DSx_max)
-TimeL = ROOT.TH1D("TimeL", "Time Distribution", 41, -20.5, 20.5)
-TimeR = ROOT.TH1D("TimeR", "Time Distribution", 41, -20.5, 20.5)
 
 Eff_Hch = ROOT.TEfficiency( "Eff_Hch", "Efficiency per DS horizontal channel; ds h channel number; veto efficiency", DSn_bins, DSx_min, DSx_max)
 Eff_Vch = ROOT.TEfficiency( "Eff_Vch", "Efficiency per DS vertical channel; ds v channel number; veto efficiency", DSn_bins, DSx_min, DSx_max)
 Eff_DS  = ROOT.TEfficiency( "Eff_ch", "Efficiency per DS channel; ds v channel number; ds h channel number", DSn_bins, DSx_min, DSx_max, DSn_bins, DSx_min, DSx_max)
-
-Residual = ROOT.TH1D("Residual", "Position residual; x_computed - dsv bar; entries", 201, -200.5, 200.5)
 
 counter = 0
 counter2 = 0
@@ -103,7 +80,11 @@ counterSameCh = 0
 counterVeto = 0
 relative_eff = 0
 
-# loop on entries
+###################
+# loop on entries #
+###################
+
+Nentries = data.GetEntries()
 for i in range(Nentries):
 #    if i%10000 == 0:
  #       print(f"Processing event {i}", end= '\r') 
@@ -120,7 +101,6 @@ for i in range(Nentries):
         # Veto boardID 48 [6,7]
         tofChannel = np.uint8(data.tofpet_channel)
         tofID = np.uint8(data.tofpet_id)
-        time = np.asarray(data.timestamp, float)
         qdc = np.asarray(data.value, float)
 
         # veto info
@@ -145,13 +125,15 @@ for i in range(Nentries):
         # DS info
         DStofID = tofID[ boardID==1]
         DStofCh = tofChannel[boardID==1]
-        DStime = time[boardID == 1]
         DSQdc = qdc[boardID == 1]
 
         DSRID = DStofID[DStofID>3]
         DSLID = DStofID[(DStofID>1) & (DStofID<4)]
         DSVID = DStofID[DStofID<2]
 
+        #########################
+        # select cosmics events #
+        #########################
 
         #ask for 3 hit in total in ds-> 1 ds R 1 ds L and 1 ds V     
 
@@ -176,6 +158,8 @@ for i in range(Nentries):
             DSRQdc.Fill(RQdc)
             DSVQdc.Fill(VQdc)
             
+            # cut on qdc to ensure is a MIP signal 
+
             if (20 < LQdc[0] < 30) and (18 < RQdc[0] < 28) and (45 < VQdc[0] < 65) and DSRBar == DSLBar: 
                 DSVHits.Fill(DSVBar)
                 DSHHits.Fill(DSLBar)
@@ -190,7 +174,8 @@ for i in range(Nentries):
 
                 for i in vetoBars :
                     VetoHits.Fill(i)
-
+                
+                Cosmic_VetoQdc.Fill(i for i in vetoQdc)
 
                 if len(vetoId)>0: 
                     counterVeto+=1
@@ -209,24 +194,35 @@ first_eff = first/10
 centre_eff = centre/10
 last_eff = last/10
 
+############################
+# write histo to root file #
+############################
+
+#hits information
 DSVHits.Write() 
 DSHHits.Write()
 VetoHits.Write()
+All_VetoHitMultiplcity.Write()
+Cosmic_VetoHitMultiplcity.Write()
 
+#efficiencies histo
 Denominatore.Write()
 Eff_Hch.Write()
 Eff_Vch.Write()
 Eff_DS.Write()
 
+#ds qdc distribution
 DSLQdc.Write()
 DSRQdc.Write()
 DSVQdc.Write()
+
+#veto qdc distribution
 VetoQdcPerBar.Write()
 VetoQDCPerChannel.Write()
 VetoQdc.Write()
+Cosmic_VetoQdc.Write()
 
-All_VetoHitMultiplcity.Write()
-Cosmic_VetoHitMultiplcity.Write()
+
 
 outfile.Close()
 
