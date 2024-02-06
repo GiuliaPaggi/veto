b00c3afcb2e18503f82f561656114faface1e3bd
gpsndlhc <giulia.paggi@cern.ch>
2024-02-06 09:51:28 +0100

    Veto 1 2 3 info plots

--- VetoEffTI18.py
+++ VetoEffTI18.py
@@ -16,6 +16,7 @@ def read_csv_file(file_path):
 def return_ch(map, tofpet_id, tofpet_channel):    
     for row in map:
         if (row[4] == str(tofpet_channel) and row[3] == str(tofpet_id%2)):
+            #print(f'pin {row[4]} tof id {row[3]} returnano {row[0]} ')
             return int(row[0])
     return -1
 
@@ -31,7 +32,7 @@ def return_bar(map, tofpet_id, tofpet_channel):
 
 runN = sys.argv[1]
 #runDirectory = f"/eos/experiment/sndlhc/raw_data/commissioning/veto/run_{runN}/"
-runDirectory = f"/eos/experiment/sndlhc/raw_data/commissioning/TI18/data/run_00{runN}/"
+runDirectory = f"/eos/experiment/sndlhc/raw_data/commissioning/2024/run_00{runN}/"
 
 
 data = ROOT.TChain("data")
@@ -52,11 +53,20 @@ outfile = ROOT.TFile.Open(filename, "RECREATE")
 # Define histos #
 #################
 
-DSn_bins = 59
+DSn_bins = 58
 DSx_min = -.5
 DSx_max = 57.5
 
 # Hits info 
+V1LHits = ROOT.TH1D("V1LHits", "V1 L Hits; veto channel; entries", DSn_bins, DSx_min, DSx_max)
+V1LHitsperBar = ROOT.TH1D("V1LHitsperBar", "V1 L Hits per Bar; veto bar; entries", 7, 0, 7)
+V1RHits = ROOT.TH1D("V1RHits", "V1 R Hits; veto channel; entries", DSn_bins, DSx_min, DSx_max)
+V1RHitsperBar = ROOT.TH1D("V1RHitsperBar", "V1 R Hits per Bar; veto bar; entries", 7, 0, 7)
+
+V2LHits = ROOT.TH1D("V2LHits", "V2 L Hits; veto channel; entries", DSn_bins, DSx_min, DSx_max)
+V2LHitsperBar = ROOT.TH1D("V2LHitsperBar", "V2 L Hits per Bar; veto bar; entries", 7, 0, 7)
+V2RHits = ROOT.TH1D("V2RHits", "V2 R Hits; veto channel; entries", DSn_bins, DSx_min, DSx_max)
+V2RHitsperBar = ROOT.TH1D("V2RHitsperBar", "V2 R Hits per Bar; veto bar; entries", 7, 0, 7)
 
 VetoHits = ROOT.TH1D("VetoHits", "Veto Hits; veto channel; entries", DSn_bins, DSx_min, DSx_max)
 VetoHitsperBar = ROOT.TH1D("VetoHitsperBar", "Veto Hits per Bar; veto bar; entries", 7, 0, 7)
@@ -115,7 +125,11 @@ PositionCut_Bar7Eff_GroupVch = ROOT.TEfficiency ( "PositionCut_Bar7Eff_GroupVch"
 #Alignment
 
 V1_vs_V2 = ROOT.TH2D("V1_vs_V2", "V1_vs_V2; v1 bar; v2 bar", 7, 0, 7, 7, 0, 7)
+V1_vs_V3 = ROOT.TH2D("V1_vs_V3", "V1_vs_V3; v1 bar; v3 bar", 7, 0, 7, 7, 0, 7)
+V2_vs_V3 = ROOT.TH2D("V2_vs_V3", "V2_vs_V3; v2 bar; v3 bar", 7, 0, 7, 7, 0, 7)
 Ch_V1_vs_V2 = ROOT.TH2D("Ch_V1_vs_V2", "Ch_V1_vs_V2; v1 channel; v2 channel", DSn_bins, DSx_min, DSx_max, DSn_bins, DSx_min, DSx_max)
+Ch_V1_vs_V3 = ROOT.TH2D("Ch_V1_vs_V3", "Ch_V1_vs_V3; v1 channel; v3 channel", DSn_bins, DSx_min, DSx_max, DSn_bins, DSx_min, DSx_max)
+Ch_V2_vs_V3 = ROOT.TH2D("Ch_V2_vs_V3", "Ch_V2_vs_V3; v2 channel; v3 channel", DSn_bins, DSx_min, DSx_max, DSn_bins, DSx_min, DSx_max)
 
 ###################
 # loop on entries #
@@ -130,7 +144,34 @@ for i in range(Nentries):
     tofID = np.uint8(data.tofpet_id)
     qdc = np.asarray(data.value, float)
 
-    # veto info
+    # first look at veto info before any cut
+    v3Id = tofID[boardID == 48]
+    v3Pin = tofChannel[boardID == 48]
+    v3Qdc = qdc[boardID == 48]
+
+    v3Multiplicity = len(v3Id)
+
+    if v3Multiplicity > 0:        
+
+        VetoHitMultiplicity.Fill(v3Multiplicity)
+
+        v3Bars = [return_bar(mapVeto, v3Id[i], v3Pin[i]) for i in range(v3Multiplicity)]
+        v3Ch = [return_ch(mapVeto, v3Id[i], v3Pin[i]) for i in range(v3Multiplicity)]
+        v3BarId = [int(np.floor((i-1)/8)) for i in v3Bars]
+
+        v3BarQDC = np.zeros(8)
+
+        for i in range(v3Multiplicity):
+                        
+            VetoHits.Fill(v3Ch[i])
+            VetoHitsperBar.Fill(v3Bars[i])
+            v3BarQDC[v3Bars[i]] += v3Qdc[i]
+            VetoQdc.Fill(v3Qdc[i])
+            VetoQDCPerChannel.Fill(v3Bars[i], v3Qdc[i])
+        
+        for i in v3BarId : Cosmic_VetoQDCPerBar.Fill(i, v3BarQDC[i])
+    
+    
     vetoId = tofID[boardID == 58]
     vetoCh = tofChannel[boardID == 58]
     vetoQdc = qdc[boardID == 58]
@@ -140,17 +181,34 @@ for i in range(Nentries):
     v2LId = vetoId[(vetoId>1) & (vetoId<4)]
     v2RId = vetoId[(vetoId>3) & (vetoId<6)]
 
+    #############################
+    # select cosmic rays events #
+    #############################
+
+    # ask hits in left and right for both the first two veto planes
+
     if len(v1LId) > 1 and len(v1RId) > 1 and len(v2LId) > 1 and len(v2RId) > 1 :       
         
         v1LPin = vetoCh[(vetoId<2)]
         v1RPin = vetoCh[(vetoId>5)]
         v2LPin = vetoCh[(vetoId>1) & (vetoId<4)]
         v2RPin = vetoCh[(vetoId>3) & (vetoId<6)]
-        
-        # v1LBar = [return_bar(mapVeto, v1LId[i], v1LCh[i]) for i in range(len(v1LId))]
-        # v1RBar = [return_bar(mapVeto, v1RId[i], v1RCh[i]) for i in range(len(v1RId))]
-        # v2LBar = [return_bar(mapVeto, v2LId[i], v2LCh[i]) for i in range(len(v2LId))]
-        # v2RBar = [return_bar(mapVeto, v2RId[i], v2RCh[i]) for i in range(len(v2RId))]
+
+        for i in range(len(v1LId)) : 
+            V1LHits.Fill(return_ch(mapVeto, v1LId[i], v1LPin[i]))
+            V1LHitsperBar.Fill(return_bar(mapVeto, v1LId[i], v1LPin[i]))
+
+        for i in range(len(v1RId)) : 
+            V1RHits.Fill(return_ch(mapVeto, v1RId[i], v1RPin[i]))
+            V1RHitsperBar.Fill(return_bar(mapVeto, v1RId[i], v1RPin[i]))
+            
+        for i in range(len(v2LId)) : 
+            V2LHits.Fill(return_ch(mapVeto, v2LId[i], v2LPin[i]))
+            V2LHitsperBar.Fill(return_bar(mapVeto, v2LId[i], v2LPin[i]))
+            
+        for i in range(len(v2RId)) : 
+            V2RHits.Fill(return_ch(mapVeto, v2RId[i], v2RPin[i]))
+            V2RHitsperBar.Fill(return_bar(mapVeto, v2RId[i], v2RPin[i]))
 
         v1LQdc = vetoQdc[(vetoId<2)]
         v1RQdc = vetoQdc[(vetoId>5)]
@@ -177,6 +235,8 @@ for i in range(Nentries):
         v2LBar = return_bar(mapVeto, v2LHitID, v2LHitPin)
         v2RBar = return_bar(mapVeto, v2RHitID, v2RHitPin)
 
+        # ask for same bar hit in left and right
+
         if v1LBar == v1RBar and v2LBar == v2RBar:
 
             v1LhitQdc = np.max(v1LQdc)
@@ -189,99 +249,32 @@ for i in range(Nentries):
             V2LQdc.Fill(v2LhitQdc)
             V2RQdc.Fill(v2RhitQdc)
 
-            V1QdcDiff.Fill( v1RhitQdc - v1LhitQdc)
-            V2QdcDiff.Fill( v2RhitQdc - v2LhitQdc)
+            V1QdcDiff.Fill(v1RhitQdc - v1LhitQdc)
+            V2QdcDiff.Fill(v2RhitQdc - v2LhitQdc)
 
             V1_vs_V2.Fill(v1LBar, v2LBar)
             Ch_V1_vs_V2.Fill(v1LCh, v2LCh)
+
+            # cut on qdc signal 
+
+            if v1LhitQdc > 18 and v1RhitQdc > 18 and v2RhitQdc > 18 and v2LhitQdc > 18:
+                
+                    for i in range(v3Multiplicity):
+                        
+                        Cosmic_VetoHits.Fill(v3Ch[i])
+                        Cosmic_VetoHitsperBar.Fill(v3BarId[i])
+                        Cosmic_VetoQdc.Fill(v3Qdc[i])
+                        Cosmic_VetoQDCPerChannel.Fill(v3Ch[i], v3Qdc[i])
+                        Ch_V1_vs_V3.Fill(v1LCh, v3Ch[i])
+                        Ch_V2_vs_V3.Fill(v2LCh, v3Ch[i])
+                    
+                    for i in v3BarId : 
+                        Cosmic_VetoQDCPerBar.Fill(i, v3BarQDC[i])
+                        V1_vs_V3.Fill(v1LBar, i)
+                        V2_vs_V3.Fill(v2LBar, i)
+
 """
-    vetoMultiplicity = len(vetoId)
     
-    if vetoMultiplicity > 0:        
-        
-        VetoHitMultiplicity.Fill(vetoMultiplicity)
-        
-        vetoBars = [return_bar(mapVeto, vetoId[i], vetoCh[i]) for i in range(vetoMultiplicity)]
-        vetoBarId = [int(np.floor((i-1)/8)) for i in vetoBars]
-
-        vetoBarQDC = np.zeros(8)
-
-        for i in range(vetoMultiplicity):
-            
-            VetoHits.Fill(vetoBars[i])
-            VetoHitsperBar.Fill(vetoBarId[i])
-            vetoBarQDC[vetoBarId[i]] += vetoQdc[i]
-            VetoQdc.Fill(vetoQdc[i])
-            VetoQDCPerChannel.Fill(vetoBars[i], vetoQdc[i])
-        
-        for i in vetoBarId : VetoQDCPerBar.Fill(i, vetoBarQDC[i])
-
-    # DS info
-    DStofID = tofID[boardID == 1]
-    DsMultiplicity = len(DStofID)
-
-    DStofCh = tofChannel[boardID == 1]
-    DSQdc = qdc[boardID == 1]
-
-    #############################
-    # select cosmic rays events #
-    #############################
-
-    if DsMultiplicity > 0:
-
-        DSRID = DStofID[DStofID>3]
-        DSLID = DStofID[(DStofID>1) & (DStofID<4)]
-        DSVID = DStofID[DStofID<2]
-
-        #########################
-        # select cosmics events #
-        #########################
-
-        #ask for 3 hit in total in ds-> 1 ds R 1 ds L and 1 ds V     
-
-        if len(DSLID) > 0 and len(DSRID) > 0 and len(DSVID) > 0:
-
-            DSLCh = DStofCh[(DStofID>1) & (DStofID<4)]
-            DSRCh = DStofCh[DStofID>3]
-            DSVCh = DStofCh[DStofID<2]
-
-            LQdc = DSQdc[(DStofID>1) & (DStofID<4)]
-            RQdc = DSQdc[DStofID>3]
-            VQdc = DSQdc[DStofID<2]
-            
-            Rqdc = np.max(RQdc)
-            Lqdc = np.max(LQdc)
-            Vqdc = np.max(VQdc)
-
-            #select hits with the highest qdc value (signal should be > noise)
-
-            if Rqdc > 0 and Lqdc > 0 and Vqdc > 0:
-
-                DSRHitCh = DSRCh[np.array(RQdc).argmax()]
-                DSLHitCh = DSLCh[np.array(LQdc).argmax()]
-                DSVHitCh = DSVCh[np.array(VQdc).argmax()]
-                
-                DSRHitID = DSRID[np.array(RQdc).argmax()]
-                DSLHitID = DSLID[np.array(LQdc).argmax()]
-                DSVHitID = DSVID[np.array(VQdc).argmax()]
-                            
-                DSRBar = return_bar(mapDS, DSRHitID, DSRHitCh)
-                DSLBar = return_bar(mapDS, DSLHitID, DSLHitCh)
-                DSVBar = return_bar(mapDS, DSVHitID, DSVHitCh)  
-
-                DSVHits.Fill(DSVBar)
-                
-                if DSRBar == DSLBar: 
-                    DSVvsHHits.Fill(DSLBar, DSVBar)
-                    DSHHits.Fill(DSLBar)
-
-                    DSLQdc.Fill(Lqdc)
-                    DSRQdc.Fill(Rqdc)
-                    DSVQdc.Fill(Vqdc)
-                    DSQdc_RvsL.Fill(Rqdc, Lqdc)
-                
-                # cut on qdc to ensure is a MIP signal 
-
                 if ( Lqdc > 35 ) and ( Rqdc > 30 ) and ( Vqdc > 15 )  and DSRBar == DSLBar:     
 
                     Denominatore.Fill(DSVBar, DSLBar)
@@ -379,9 +372,7 @@ for i in range(Nentries):
 ############################
 
 #hits information
-DSVHits.Write() 
-DSHHits.Write()
-DSVvsHHits.Write()
+
 VetoHits.Write()
 VetoHitsperBar.Write()
 VetoHitMultiplicity.Write()
@@ -391,11 +382,7 @@ Noise_VetoHits.Write()
 Noise_VetoMultiplicity.Write()
 Simp_perBar.Write()
 
-#efficiencies histo
-Denominatore.Write()
-Eff_Hch.Write()
-Eff_Vch.Write()
-Eff_DS.Write()
+
 
 PositionCut_Eff_Hch.Write()
 PositionCut_Eff_Vch.Write()
@@ -408,12 +395,6 @@ PositionCut_Bar3Eff_GroupVch.Write()
 PositionCut_Bar4Eff_GroupVch.Write()
 PositionCut_Bar5Eff_GroupVch.Write()
 PositionCut_Bar6Eff_GroupVch.Write()
-
-#ds qdc distribution
-DSLQdc.Write()
-DSRQdc.Write()
-DSVQdc.Write()
-DSQdc_RvsL.Write()
 
 #veto qdc distribution
 VetoQDCPerPosition.Write()
@@ -433,12 +414,12 @@ Cosmic_VetoQDCPerBar.Write()
 Bkg_VetoQdc.Write()
 Noise_VetoQdc.Write()
 
-# relative alignment
-DsH_vs_Veto.Write()
-DsV_vs_Veto.Write()
-
 
 """
+
+############################
+# write histo to root file #
+############################
 
 V1LQdc.Write()
 V1RQdc.Write()
@@ -449,6 +430,31 @@ V1QdcDiff.Write()
 V2QdcDiff.Write()
 
 V1_vs_V2.Write()
+V1_vs_V3.Write()
+V2_vs_V3.Write()
 Ch_V1_vs_V2.Write()
+Ch_V1_vs_V3.Write()
+Ch_V2_vs_V3.Write()
+
+V1LHits.Write()
+V1LHitsperBar.Write()
+V1RHits.Write()
+V1RHitsperBar.Write()
+V2LHits.Write()
+V2LHitsperBar.Write()
+V2RHits.Write()
+V2RHitsperBar.Write()
+
+VetoHits.Write()
+VetoHitsperBar.Write()
+VetoQdc.Write()
+VetoQDCPerChannel.Write()
+VetoQDCPerBar.Write()
+
+Cosmic_VetoHits.Write()
+Cosmic_VetoHitsperBar.Write()
+Cosmic_VetoQdc.Write()
+Cosmic_VetoQDCPerChannel.Write()
+Cosmic_VetoQDCPerBar.Write()
 
 outfile.Close()
