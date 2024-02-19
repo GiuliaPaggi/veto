5be696843cc126e5f15dbaf3a349e350ac2403ee
gpsndlhc <giulia.paggi@cern.ch>
2024-02-19 14:15:41 +0100

    Script to add condor output

---
+++ add.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+dir="/afs/cern.ch/work/g/gpsndlhc/veto/results"
+files=()
+
+for file in "$dir"/output_*; do
+    if [ -f "$file" ]; then
+        size=$(stat -c %s "$file")
+        if (( $size > 1000 )); then 
+            files+=("$file")
+            #hadd -f "$dir"/prova.root $file
+        fi
+    fi
+done
+
+hadd -f "$dir"/prova.root ${files[@]}
