4a040047cc51458f946904ff2ef66931f39d7500
gpsndlhc <giulia.paggi@cern.ch>
2024-02-23 11:49:11 +0100

    remove condor log once finished

--- add.sh
+++ add.sh
@@ -2,15 +2,26 @@
 
 dir="/afs/cern.ch/work/g/gpsndlhc/veto/results"
 files=()
+unfinished=0
 
 for file in "$dir"/runs/output_*; do
     if [ -f "$file" ]; then
         size=$(stat -c %s "$file")
-        if (( $size > 1000 )); then 
+        if (( $size > 100 )); then 
             files+=("$file")
             #hadd -f "$dir"/prova.root $file
+        else 
+            unfinished=$((unfinished+1))
         fi
     fi
 done
 
 hadd -f "$dir"/analisysResult.root ${files[@]}
+
+echo $unfinished
+if [ $unfinished -eq 0 ]; then
+    echo Removing condor job log files
+    rm /afs/cern.ch/work/g/gpsndlhc/veto/condorJob/error/VetoAnalysis.*
+    rm /afs/cern.ch/work/g/gpsndlhc/veto/condorJob/error/VetoAnalysis.*
+    rm /afs/cern.ch/work/g/gpsndlhc/veto/condorJob/error/VetoAnalysis.* 
+fi
