215ca24b380c3e83844b6f54889cb1c9e180ebef
gpsndlhc <giulia.paggi@cern.ch>
2024-02-23 14:52:30 +0100

    argument to remove old files

--- add.sh
+++ add.sh
@@ -2,14 +2,12 @@
 
 dir="/afs/cern.ch/work/g/gpsndlhc/veto/results"
 files=()
-unfinished=0
-
+# unfinished=0
 for file in "$dir"/runs/output_*; do
     if [ -f "$file" ]; then
         size=$(stat -c %s "$file")
         if (( $size > 1000 )); then 
             files+=("$file")
-            #hadd -f "$dir"/prova.root $file
         else 
             unfinished=$((unfinished+1))
         fi
@@ -18,8 +16,8 @@ done
 
 hadd -f "$dir"/analisysResult.root ${files[@]}
 
-echo $unfinished
-if [ $unfinished -eq 0 ]; then
+# echo $unfinished
+if [ "$1" = "-r" ]; then
     echo Removing condor job log files
     rm /afs/cern.ch/work/g/gpsndlhc/veto/condorJob/error/VetoAnalysis.*
     rm /afs/cern.ch/work/g/gpsndlhc/veto/condorJob/error/VetoAnalysis.*
