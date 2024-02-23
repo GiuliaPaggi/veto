5fa5f225da2e4bc735000f8a9ce5867210e2c8e8
gpsndlhc <giulia.paggi@cern.ch>
2024-02-23 14:51:48 +0100

    Fixed for ti18 output

--- RelativeEff.py
+++ RelativeEff.py
@@ -1,17 +1,14 @@
-import csv 
 import sys
 from glob import glob
 from ROOT import TFile, TChain, gROOT, TCanvas, TLegend, gPad
 import ROOT
 
-nRun=sys.argv[1]
-
-f = ROOT.TFile.Open(f'./results/output{nRun}.root', 'read')
-eff = f.Get('PositionCut_Eff_GroupVch')
+f = ROOT.TFile.Open(f'./results/analisysResult.root', 'read')
+eff = f.Get('Close_vs_farEfficiency')
 barEff = [None]*7
 relBarEff = [0]*7
 for i in range(7):
-    barEff[i]= f.Get(f'PositionCut_Bar{i}Eff_GroupVch')
+    barEff[i]= f.Get(f'Close_vs_farEfficiency_Bar{i}')
 
 # compute relative efficiency
 
@@ -20,28 +17,29 @@ relEff = eff.GetEfficiency(1)/eff.GetEfficiency(3)
 for i in range(7):
     relBarEff[i] = barEff[i].GetEfficiency(1)/barEff[i].GetEfficiency(3)
 
-with open(f'./results/RelEff_run{nRun}.txt', 'w') as f:
+with open(f'./results/RelEff.txt', 'w') as f:
     f.write(f'Veto relative efficiency is {relEff}')
     for i in range(7) :
         f.write(f'\nVeto bar {i} relative efficiency is {relBarEff[i]}')
 
 # draw superimposed 7 bars
-colors = [ROOT.kRed,ROOT.kGreen,ROOT.kBlue,ROOT.kMagenta,ROOT.kCyan,ROOT.kOrange,ROOT.kSpring,ROOT.kTeal,ROOT.kViolet,ROOT.kYellow]
+colors = [ROOT.kRed,ROOT.kGreen,ROOT.kBlue,ROOT.kMagenta,ROOT.kCyan,ROOT.kOrange,ROOT.kOrange+3,ROOT.kTeal,ROOT.kSpring,ROOT.kViolet,ROOT.kYellow]
 c = ROOT.TCanvas(f"Relative Eff Bars", f"Relative Eff Bars")
-l = ROOT.TLegend(0.1, 0.7, 0.3, 0.9)
+l = ROOT.TLegend(0.1, 0.1, 0.3, 0.4)
 
+l.AddEntry(eff, "all")
 for i in range(7):
     barEff[i].SetLineColor(colors[i])
     l.AddEntry(barEff[i], f"Bar {i}")
 
 
-barEff[0].Draw()
+eff.Draw()
 gPad.Update()
-graph = barEff[0].GetPaintedGraph()
+graph = eff.GetPaintedGraph()
 graph.SetMinimum(0)
 graph.SetMaximum(1) 
 gPad.Update()
-for i in range(1, 7):
+for i in range(0, 7):
     barEff[i].Draw("same")
 l.Draw("same")
 c.SaveAs("./images/relEff.png")
